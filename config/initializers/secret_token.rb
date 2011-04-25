8775cb749eee7b0004c4dba082e3e6371954b09f
raghavan <raghavan@krishna.(none)>
2011-04-25 13:42:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saaku20::Application.config.secret_token = 'ebc91c638a8da518a601c2268510b7bd711901669ef2e8e748d052f5f9f5155073894bbe68e828fcc4c0994d57cfd93c80cdb0d3178e2a95e50bb868784bac7a'
