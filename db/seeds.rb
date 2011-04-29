17713baa47ba11b00f23018a1da63460e2257751
Shreyas Satish <shreyas@mapunity.in>
2011-04-29 18:15:30 +0530

    Added seeds to help kickstart app with a sample user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+user = User.create(:email => 'admin@saaku.in', :password => 'admin123', :password_confirmation => 'admin123')
+role = Role.create(:name => 'admin')
+user.roles << role
+user.save
+
