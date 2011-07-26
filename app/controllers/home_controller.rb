abe8b81346452cb776f158a5aa14b231209d84e8
raghavan <raghavan@krishna.(none)>
2011-07-26 14:13:35 +0530

    Cleaned up the home page to fit the events and the highlights points well. Added the survey box in the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @upcoming_events = Event.upcoming.order('start_date ASC')
     @recent_events = Event.recent
+    @recent_surveys = Survey.recent
   end
 
   
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,4 +3,8 @@ class Survey < ActiveRecord::Base
 
   has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
+
+  def self.recent
+    Survey.all #where("ends_on <= ?", Time.now())
+  end
 end
